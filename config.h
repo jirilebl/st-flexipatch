dfa6a873dabb1ee1dd721c10f21eb3adcddb795e
Jiri (George) Lebl <jiri.lebl@gmail.com>
2026-05-23 23:43:35 -0500

    Easier on the eyes background

--- config.h
+++ config.h
@@ -211,7 +211,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#000000", /* 258 -> bg */
+	"#121212", /* 258 -> bg */
 	"#e5e5e5", /* 259 -> fg */
 };
 
